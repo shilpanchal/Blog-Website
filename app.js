64067ac786e9d3ed2b7b7b8857a33ce49f679f60
Shilpanchal <shilpapanchal009@gmail.com>
2021-06-20 00:32:26 +0530

    blog website final copy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
+let posts =[];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,9 +17,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/", function(req,res){
-  res.render("home")
+  res.render("home", {homeStartingContent : homeStartingContent , posts : posts})
+})
+app.get("/about", function(req,res){
+  res.render("about", {aboutContent : aboutContent})
+})
+app.get("/contact", function(req,res){
+  res.render("contact", {contactContent : contactContent})
+})
+app.get("/compose", function(req,res){
+  res.render("compose")
+})
+app.get("/post/:postName", function(req,res){
+  posts.forEach(function(post){
+    if(_.lowerCase(post.title) === _.lowerCase(req.params.postName))
+    {
+      res.render("post" , {title : post.title, content : post.content})
+    }
+  })
 })
 
+app.post("/compose", function(req,res){
+  const post = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  }
+  posts.push(post)
+ res.redirect("/")
+})
 
 
 
